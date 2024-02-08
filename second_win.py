4be6fa26a5a7da3147d8c6ef763c2c696300db64
Javar <55617502+javarcompany@users.noreply.github.com>
2024-02-08 16:14:50 +0300

    add set_appear to second screen

--- second_win.py
+++ second_win.py
@@ -68,4 +68,7 @@ class TestEnv(QWidget):
 
         self.setLayout(self.h_line)
 
-        
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
