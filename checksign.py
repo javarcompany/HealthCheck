04fa8a43cf085b62cddf948827ecdd0e4b97c025
Javar <55617502+javarcompany@users.noreply.github.com>
2024-02-09 08:26:08 +0300

    Add Sign File

---
+++ checksign.py
@@ -0,0 +1,10 @@
+text = 'algo'
+for sign in text:
+    if sign == 'a':
+        print('*')
+    elif sign == 'g':
+        print('^')
+    elif sign == 'o':
+        break
+    else:
+        print(sign)
